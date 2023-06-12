543882a4c1474c2a8f1e6dc24c3c1b6a47120d98
Tom Glastonbury <tom@postsharp.net>
2023-06-12 16:00:12 +0100

    [Required], also InterfaceTests, which fail.
    InterfaceTests was the only test class to mention [Required]. It actually asserts that contracts placed on interface members should propagate to interface implementations, but this does not happen.

---
+++ src/Metalama.Patterns.Contracts/RequiredAttribute.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using Metalama.Framework.Eligibility;
+
+namespace Metalama.Patterns.Contracts;
+
+/// <summary>
+/// Custom attribute that, when added to a field, property or parameter, throws:
+/// <list type="bullet">
+///     <item><description>
+///         an <see cref="ArgumentNullException"/> if the target is assigned a null value;
+///     </description></item>
+///     <item><description>
+///         an <see cref="ArgumentOutOfRangeException"/> if the target is assigned an empty or white-space string.
+///     </description></item>
+/// </list>
+/// </summary>
+/// <remarks>
+/// <para>Error message is identified by <see cref="ContractLocalizedTextProvider.RequiredErrorMessage"/>.</para>
+/// </remarks>
+public sealed class RequiredAttribute : ContractAspect
+{
+    public override void BuildEligibility( IEligibilityBuilder<IFieldOrPropertyOrIndexer> builder )
+    {
+        base.BuildEligibility( builder );
+        builder.MustSatisfy(
+            f => f.Type.IsReferenceType != false || f.Type.IsNullable != false,
+            f => $"the type of {f} must be a nullable" );
+    }
+
+    public override void BuildEligibility( IEligibilityBuilder<IParameter> builder )
+    {
+        base.BuildEligibility( builder );
+        builder.MustSatisfy(
+            p => p.Type.IsReferenceType != false || p.Type.IsNullable != false,
+            p => $"the type of {p} must be a nullable" );
+    }
+
+    public override void Validate( dynamic? value )
+    {
+        CompileTimeHelpers.GetTargetKindAndName( meta.Target, out var targetKind, out var targetName );
+
+        var targetType = CompileTimeHelpers.GetTargetType( meta.Target );
+
+        if ( targetType.SpecialType == SpecialType.String )
+        {
+            if ( string.IsNullOrWhiteSpace( value ) )
+            {
+                if ( value == null! )
+                {
+                    throw ContractServices.ExceptionFactory.CreateException( ContractExceptionInfo.Create(
+                        typeof( ArgumentNullException ),
+                        typeof( RequiredAttribute ),
+                        value,
+                        targetName,
+                        targetKind,
+                        meta.Target.ContractDirection,
+                        ContractLocalizedTextProvider.RequiredErrorMessage ) );
+                }
+                else
+                {
+                    throw ContractServices.ExceptionFactory.CreateException( ContractExceptionInfo.Create(
+                        typeof( ArgumentOutOfRangeException ),
+                        typeof( RequiredAttribute ),
+                        value,
+                        targetName,
+                        targetKind,
+                        meta.Target.ContractDirection,
+                        ContractLocalizedTextProvider.RequiredErrorMessage ) );
+                }
+            }
+        }
+        else
+        {
+            if ( value == null! )
+            {
+                throw ContractServices.ExceptionFactory.CreateException( ContractExceptionInfo.Create(
+                    typeof( ArgumentNullException ),
+                    typeof( RequiredAttribute ),
+                    value,
+                    targetName,
+                    targetKind,
+                    meta.Target.ContractDirection,
+                    ContractLocalizedTextProvider.RequiredErrorMessage ) );
+            }
+        }
+    }
+}
--- src/tests/Metalama.Patterns.Contracts.UnitTests/InterfaceTests.cs
+++ src/tests/Metalama.Patterns.Contracts.UnitTests/InterfaceTests.cs
@@ -1,41 +1,32 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. This file is not open source. It is released under a commercial
 // source-available license. Please see the LICENSE.md file in the repository root for details.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using PostSharp.Patterns.Common.Tests;
-using PostSharp.Patterns.Common.Tests.Helpers;
+using Metalama.Patterns.Tests.Helpers;
 using Xunit;
 
-using PostSharp.Patterns.Contracts;
+namespace Metalama.Patterns.Contracts.Tests;
 
-namespace PostSharp.Patterns.Contracts.Tests
+public class InterfaceTests
 {
-    public class InterfaceTests
+    // TODO: Review. This test fails.
+    [Fact]
+    public void TestInterfaceImpl()
     {
-        [Fact]
-        public void TestInterfaceImpl()
-        {
-            AssertEx.Throws<ArgumentNullException>( () =>
-             {
-                 Foo f = new Foo();
-                 f.Bar( null );
-             } );
-        }
+        var cut = new Foo();
+        var e = TestHelpers.RecordException<ArgumentNullException>( () => cut.Bar( null! ) );
+        Assert.NotNull( e );
+    }
+    
+    interface IFoo
+    {
+        void Bar( [Required] string a );
+    }
 
-        interface IFoo
+    class Foo : IFoo
+    {
+        public void Bar( string a )
         {
-            void Bar( [Required] string a );
-        }
-
-        class Foo : IFoo
-        {
-            public void Bar( string a )
-            {
-                
-            }
+            
         }
     }
 }
