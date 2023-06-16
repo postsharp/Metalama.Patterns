865f7dd75197ead612c96264f520a795b6daccff
Tom Glastonbury <tom@postsharp.net>
2023-06-16 17:12:28 +0100

    Add aspect tests for [NotNull] eligibility.

---
+++ src/tests/Metalama.Patterns.Contracts.AspectTests/NotNull_Ineligible_Int.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+namespace Metalama.Patterns.Contracts.AspectTests;
+
+public class NotNull_Ineligible_Int
+{
+    [NotNull]
+    private int field;
+}
