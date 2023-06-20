d7abd72cc0a295348f83bf5850a4253c90fd5c6b
Tom Glastonbury <tom@postsharp.net>
2023-06-20 07:49:38 +0100

    Show theory test cases properly in Visual Studio.

--- src/tests/Flashtrace.Formatters.UnitTests/FormatterOverwritingTest.cs
+++ src/tests/Flashtrace.Formatters.UnitTests/FormatterOverwritingTest.cs
@@ -14,148 +14,149 @@ public class FormatterOverwritingTest : FormattersTestsBase
     {
     }
 
-    public record TestCase( Type TValue, Type OldFormatterTargetType, Type OldFormatterType, Type NewFormatterTargetType, Type NewFormatterType, string Description )
+    public record TestCase( string Description, Type TValue, Type OldFormatterTargetType, Type OldFormatterType, Type NewFormatterTargetType, Type NewFormatterType )
     {
-        public override string ToString()
-            => $"{this.Description} <{this.TValue.Name}>( {this.OldFormatterTargetType.Name}, {this.OldFormatterType.Name}, {this.NewFormatterTargetType.Name}, {this.NewFormatterType.Name} )";
+        public override string ToString() => this.Description;
     }
 
-    private static TestCase Case<TValue>( string description, Type oldFormatterTargetType, Type oldFormatterType, Type newFormatterTargetType, Type newFormatterType ) =>
-        new TestCase( typeof( TValue ), oldFormatterTargetType, oldFormatterType, newFormatterTargetType, newFormatterType, description );
-
-    private static IEnumerable<object[]> MakeBeforeBetweenPermutations( params TestCase[] testCases )
+    private static IEnumerable<object[]> MakeSerializableBeforeBetweenPermutations( IEnumerable<TestCase> testCases )
     {
         foreach ( var testCase in testCases )
         {
-            yield return new object[] { testCase, false, false };
-            yield return new object[] { testCase, false, true };
-            yield return new object[] { testCase, true, false };
-            yield return new object[] { testCase, true, true };
+            yield return new object[] { testCase.ToString(), false, false };
+            yield return new object[] { testCase.ToString(), false, true };
+            yield return new object[] { testCase.ToString(), true, false };
+            yield return new object[] { testCase.ToString(), true, true };
         }
     }
 
-    public static IEnumerable<object[]> EnsureOverwritesTestCases() => MakeBeforeBetweenPermutations(
+    public static IEnumerable<TestCase> EnsureOverwritesTestCases() => new TestCase[] {
         // object -> object
-        Case<int[]>( 
-            "object -> object",
+        new(
+            "object -> object", typeof( int[] ),
             typeof( object ), typeof( ZeroFormatter<object> ),
             typeof( object ), typeof( OneFormatter<object> ) ),
 
         // object -> generic interface
-        Case<int[]>( 
-            "object -> generic interface",
+        new(
+            "object -> generic interface", typeof( int[] ),
             typeof( object ), typeof( ZeroFormatter<object> ),
             typeof( IEnumerable<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic interface -> generic interface (same)
-        Case<int[]>( 
-            "generic interface -> generic interface (same)",
+        new(
+            "generic interface -> generic interface (same)", typeof( int[] ),
             typeof( IEnumerable<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( IEnumerable<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic interface -> generic interface (better)
-        Case<int[]>( 
-            "generic interface -> generic interface (better)",
+        new(
+            "generic interface -> generic interface (better)", typeof( int[] ),
             typeof( IEnumerable<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( ICollection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic interface -> generic base type (better)
-        Case<ObservableCollection<int>>( 
-            "generic interface -> generic base type (better)",
+        new(
+            "generic interface -> generic base type (better)", typeof( ObservableCollection<int>  ),
             typeof( IEnumerable<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( Collection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic base type -> generic base type (same)
-        Case<ObservableCollection<int>>( 
-            "generic base type -> generic base type (same)",
+        new(
+            "generic base type -> generic base type (same)", typeof( ObservableCollection<int> ),
             typeof( Collection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( Collection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic base type -> generic base type (better)
-        Case<MyObservableCollection<int>>( 
-            "generic base type -> generic base type (better)",
+        new(
+            "generic base type -> generic base type (better)", typeof( MyObservableCollection<int> ),
             typeof( Collection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( ObservableCollection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic base type -> generic exact type
-        Case<ObservableCollection<int>>( 
-            "generic base type -> generic exact type",
+        new(
+            "generic base type -> generic exact type", typeof( ObservableCollection<int> ),
             typeof( Collection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( ObservableCollection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic exact type -> generic exact type
-        Case<ObservableCollection<int>>( 
-            "generic exact type -> generic exact type",
+        new(
+            "generic exact type -> generic exact type", typeof( ObservableCollection<int> ),
             typeof( ObservableCollection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( ObservableCollection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // interface -> generic exact type
-        Case<ObservableCollection<int>>( 
-            "interface -> generic exact type",
+        new(
+            "interface -> generic exact type", typeof( ObservableCollection<int> ),
             typeof( IEnumerable<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( ObservableCollection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // interface -> interface (same)
-        Case<ObservableCollection<int>>( 
-            "interface -> interface (same)",
+        new(
+            "interface -> interface (same)", typeof( ObservableCollection<int> ),
             typeof( IEnumerable<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( IEnumerable<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // interface -> interface (better)
-        Case<ObservableCollection<int>>( 
-            "interface -> interface (better)",
+        new(
+            "interface -> interface (better)", typeof( ObservableCollection<int> ),
             typeof( IEnumerable<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( ICollection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // interface -> base type (better)
-        Case<ObservableCollection<int>>( 
-            "interface -> base type (better)",
+        new(
+            "interface -> base type (better)", typeof( ObservableCollection<int> ),
             typeof( IEnumerable<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( Collection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // base type -> base type (same)
-        Case<ObservableCollection<int>>( 
-            "base type -> base type (same)",
+        new(
+            "base type -> base type (same)", typeof( ObservableCollection<int> ),
             typeof( Collection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( Collection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // base type -> base type (better)
-        Case<MyObservableCollection<int>>( 
-            "base type -> base type (better)",
+        new(
+            "base type -> base type (better)", typeof( MyObservableCollection<int> ),
             typeof( Collection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( ObservableCollection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // base type -> exact type
-        Case<ObservableCollection<int>>( 
-            "base type -> exact type",
+        new(
+            "base type -> exact type", typeof( ObservableCollection<int> ),
             typeof( Collection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( ObservableCollection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // exact type -> exact type
-        Case<ObservableCollection<int>>( 
-            "exact type -> exact type",
+        new(
+            "exact type -> exact type", typeof( ObservableCollection<int> ),
             typeof( ObservableCollection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( ObservableCollection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // Array (base type) -> Array (generic base type)
-        Case<int[]>( 
-            "Array (base type) -> Array (generic base type)",
+        new(
+            "Array (base type) -> Array (generic base type)", typeof( int[] ),
             typeof( Array ), typeof( ZeroFormatter<Array> ),
-            typeof( Array ), typeof( OneEnumerableFormatter<> ) ) );
+            typeof( Array ), typeof( OneEnumerableFormatter<> ) )
+    };
 
-    [MemberData( nameof( EnsureOverwritesTestCases ) )]
+    public static IEnumerable<object[]> EnsureOverwritesSerializablePermutations() => MakeSerializableBeforeBetweenPermutations( EnsureOverwritesTestCases() );
+
+    [MemberData( nameof( EnsureOverwritesSerializablePermutations ) )]
     [Theory]
-    public void EnsureOverwrites( TestCase testCase, bool logBefore, bool logBetween )
+    public void EnsureOverwrites( string testCase, bool logBefore, bool logBetween )
     {
-        typeof( FormatterOverwritingTest ).GetMethod( nameof(this.EnsureOverwritesCore ), BindingFlags.Instance | BindingFlags.NonPublic ).MakeGenericMethod( testCase.TValue )
+        var testCaseRecord = EnsureOverwritesTestCases().Single( t => t.Description == testCase );
+
+        typeof( FormatterOverwritingTest ).GetMethod( nameof( this.EnsureOverwritesCore ), BindingFlags.Instance | BindingFlags.NonPublic ).MakeGenericMethod( testCaseRecord.TValue )
             .Invoke(
                 this,
                 new object[]
                 {
-                    testCase.OldFormatterTargetType,
-                    testCase.OldFormatterType,
-                    testCase.NewFormatterTargetType,
-                    testCase.NewFormatterType,
+                    testCaseRecord.OldFormatterTargetType,
+                    testCaseRecord.OldFormatterType,
+                    testCaseRecord.NewFormatterTargetType,
+                    testCaseRecord.NewFormatterType,
                     logBefore,
                     logBetween
                 } );
@@ -195,75 +196,91 @@ public class FormatterOverwritingTest : FormattersTestsBase
         Assert.Equal( newExpectedOutput, result );
     }
 
-    public static IEnumerable<object[]> EnsureDoesntOverwriteTestCases() => MakeBeforeBetweenPermutations(
+    public static IEnumerable<TestCase> EnsureDoesntOverwriteTestCases() => new TestCase[] {
         // generic interface -> object
-        Case<int[]>( "generic interface -> object",
+        new(
+            "generic interface -> object", typeof( int[] ),
             typeof( IEnumerable<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( object ), typeof( OneFormatter<object> ) ),
 
         // generic interface -> generic interface (worse)
-        Case<int[]>( "generic interface -> generic interface (worse)",
+        new(
+            "generic interface -> generic interface (worse)", typeof( int[] ),
             typeof( ICollection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( IEnumerable<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic base type -> generic interface (worse)
-        Case<ObservableCollection<int>>( "generic base type -> generic interface (worse)",
+        new(
+            "generic base type -> generic interface (worse)", typeof(ObservableCollection<int> ),
             typeof( Collection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( IEnumerable<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic base type -> generic base type (worse)
-        Case<MyObservableCollection<int>>( "generic base type -> generic base type (worse)",
+        new(
+            "generic base type -> generic base type (worse)", typeof(MyObservableCollection<int>),
             typeof( ObservableCollection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( Collection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic exact type -> generic base type
-        Case<ObservableCollection<int>>( "generic exact type -> generic base type",
+        new(
+            "generic exact type -> generic base type", typeof(ObservableCollection<int> ),
             typeof( ObservableCollection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( Collection<> ), typeof( OneEnumerableFormatter<> ) ),
 
         // generic exact type -> interface
-        Case<ObservableCollection<int>>( "generic exact type -> interface",
+        new(
+            "generic exact type -> interface", typeof(ObservableCollection<int> ),
             typeof( ObservableCollection<> ), typeof( ZeroEnumerableFormatter<> ),
             typeof( IEnumerable<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // interface -> interface (worse)
-        Case<ObservableCollection<int>>( "interface -> interface (worse)",
+        new(
+            "interface -> interface (worse)", typeof(ObservableCollection<int> ),
             typeof( ICollection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( IEnumerable<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // base type -> interface (worse)
-        Case<ObservableCollection<int>>( "base type -> interface (worse)",
+        new(
+            "base type -> interface (worse)", typeof(ObservableCollection<int> ),
             typeof( Collection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( IEnumerable<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // base type -> base type (worse)
-        Case<MyObservableCollection<int>>( "base type -> base type (worse)",
+        new(
+            "base type -> base type (worse)",typeof(MyObservableCollection<int>),
             typeof( ObservableCollection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( Collection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // exact type -> base type
-        Case<ObservableCollection<int>>( "exact type -> base type",
+        new(
+            "exact type -> base type", typeof(ObservableCollection<int> ),
             typeof( ObservableCollection<int> ), typeof( ZeroEnumerableFormatter<int> ),
             typeof( Collection<int> ), typeof( OneEnumerableFormatter<int> ) ),
 
         // Array (generic base type) -> Array (base type)
-        Case<int[]>( "Array (generic base type) -> Array (base type)",
+        new(
+            "Array (generic base type) -> Array (base type)",typeof( int[] ),
             typeof( Array ), typeof( ZeroEnumerableFormatter<> ),
-            typeof( Array ), typeof( OneFormatter<Array> ) ) );
+            typeof( Array ), typeof( OneFormatter<Array> ) )
+    };
 
-    [MemberData( nameof( EnsureDoesntOverwriteTestCases ) )]
+    public static IEnumerable<object[]> EnsureDoesntOverwritesSerializablePermutations() => MakeSerializableBeforeBetweenPermutations( EnsureDoesntOverwriteTestCases() );
+
+    [MemberData( nameof( EnsureDoesntOverwritesSerializablePermutations ) )]
     [Theory]
-    public void EnsureDoesntOverwrite( TestCase testCase, bool logBefore, bool logBetween )
+    public void EnsureDoesntOverwrite( string testCase, bool logBefore, bool logBetween )
     {
-        typeof( FormatterOverwritingTest ).GetMethod( nameof( this.EnsureDoesntOverwriteCore ), BindingFlags.Instance | BindingFlags.NonPublic ).MakeGenericMethod( testCase.TValue )
+        var testCaseRecord = EnsureDoesntOverwriteTestCases().Single( t => t.Description == testCase );
+
+        typeof( FormatterOverwritingTest ).GetMethod( nameof( this.EnsureDoesntOverwriteCore ), BindingFlags.Instance | BindingFlags.NonPublic ).MakeGenericMethod( testCaseRecord.TValue )
             .Invoke(
                 this,
                 new object[]
                 {
-                    testCase.OldFormatterTargetType,
-                    testCase.OldFormatterType,
-                    testCase.NewFormatterTargetType,
-                    testCase.NewFormatterType,
+                    testCaseRecord.OldFormatterTargetType,
+                    testCaseRecord.OldFormatterType,
+                    testCaseRecord.NewFormatterTargetType,
+                    testCaseRecord.NewFormatterType,
                     logBefore,
                     logBetween
                 } );
