ebc45f1d353743ad28c4159815b295a4732d4fbb
Tom Glastonbury <tom@postsharp.net>
2023-06-22 12:15:32 +0100

    Update tests, all tests passing.

--- src/tests/Flashtrace.UnitTests/LogEventDataTest.cs
+++ src/tests/Flashtrace.UnitTests/LogEventDataTest.cs
@@ -1,135 +1,130 @@
-﻿using PostSharp.Patterns.Contracts;
-using PostSharp.Patterns.Diagnostics;
-using PostSharp.Patterns.Diagnostics.Custom;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
 using Xunit;
 
-namespace PostSharp.Patterns.Common.Tests.Diagnostics
+namespace Flashtrace.UnitTests;
+
+public class LogEventDataTests
 {
-    public class LogEventDataTests
+    [Fact]
+    public void TestAnonymous()
     {
-        [Fact]
-        public void TestAnonymous()
+        var data = LogEventData.Create( new { Name1 = "Value", Name2 = 2 } );
+
+        var properties = data.ToDictionary();
+
+        Assert.Equal( 2, properties.Count );
+        Assert.Equal( "Value", properties["Name1"] );
+        Assert.Equal( 2, properties["Name2"] );
+
+    }
+
+    [Fact]
+    public void TestLegacy()
+    {
+        var inputProperties = new[] { new LoggingProperty( "Name1", "Value" ), new LoggingProperty( "Name2", 2 ) };
+        var data = new LogEventData( inputProperties );
+
+        var properties = data.ToDictionary();
+
+        Assert.Equal( 2, properties.Count );
+        Assert.Equal( "Value", properties["Name1"] );
+        Assert.Equal( 2, properties["Name2"] );
+    }
+
+    [Fact]
+    public void TestWithExpressionModel()
+    {
+        var data = LogEventData.Create( new object(), new TestMetadata() );
+
+        Assert.NotNull( data.GetExpressionModel<TestExpressionModel>() );
+    }
+
+    [Fact]
+    public void TestAugmented()
+    {
+        var data = LogEventData.Create( new { Name1 = "Value", Name2 = 2 } ).WithAdditionalProperty( "Name3", 1.2 );
+
+        var properties = data.ToDictionary();
+
+        Assert.Equal( 3, properties.Count );
+        Assert.Equal( "Value", properties["Name1"] );
+        Assert.Equal( 2, properties["Name2"] );
+        Assert.Equal( 1.2, properties["Name3"] );
+    }
+
+    [Fact]
+    public void TestAugmentedEmpty()
+    {
+        var data = default( LogEventData ).WithAdditionalProperty( "Name3", 1.2 );
+
+        var properties = data.ToDictionary();
+
+        Assert.Equal( 1, properties.Count );
+        Assert.Equal( 1.2, properties["Name3"] );
+    }
+
+    [Fact]
+    public void TestAugmentedExpressionModel()
+    {
+        var rawData = new object();
+        var data = LogEventData.Create( rawData, new TestMetadata() ).WithAdditionalProperty( "Name3", 1.2 );
+
+        var expressionModel = data.GetExpressionModel<TestExpressionModel>();
+        Assert.NotNull( expressionModel );
+        Assert.Same( rawData, expressionModel.Data );
+    }
+
+
+    [Fact]
+    public void TestAnnotatedProperties()
+    {
+        var o = new PropertiesWithAttributes();
+        var eventData = LogEventData.Create( o );
+
+        // Test the dynamic factory.
+        Assert.Same( eventData.Metadata, LogEventData.Create( o, null ).Metadata );
+
+        Assert.True( eventData.Metadata.GetPropertyOptions( "Inherited" ).IsInherited );
+        Assert.True( eventData.Metadata.GetPropertyOptions( "Ignored" ).IsIgnored );
+
+        var properties = eventData.ToDictionary();
+
+        Assert.Equal( 1, properties.Count );
+
+    }
+
+    class TestExpressionModel
+    {
+        public readonly object Data;
+
+        public TestExpressionModel( object data )
         {
-            LogEventData data = LogEventData.Create( new { Name1 = "Value", Name2 = 2 } );
+            this.Data = data;
+        }
+    }
 
-            IReadOnlyDictionary<string, object> properties = data.ToDictionary();
-
-            Assert.Equal( 2, properties.Count );
-            Assert.Equal( "Value", properties["Name1"] );
-            Assert.Equal( 2, properties["Name2"] );
-
+    private class TestMetadata : LogEventMetadata<TestExpressionModel>
+    {
+        public TestMetadata() : base( "Test" )
+        {
         }
 
-        [Fact]
-        public void TestLegacy()
+        public override TestExpressionModel GetExpressionModel( object data )
         {
-            LoggingProperty[] inputProperties = new[] { new LoggingProperty( "Name1", "Value" ), new LoggingProperty( "Name2", 2 ) };
-            LogEventData data = new LogEventData( inputProperties );
-
-            IReadOnlyDictionary<string, object> properties = data.ToDictionary();
-
-            Assert.Equal( 2, properties.Count );
-            Assert.Equal( "Value", properties["Name1"] );
-            Assert.Equal( 2, properties["Name2"] );
+            return new TestExpressionModel( data );
         }
 
-        [Fact]
-        public void TestWithExpressionModel()
-        {
-            LogEventData data = LogEventData.Create( new object(), new TestMetadata() );
+    }
 
-            Assert.NotNull( data.GetExpressionModel<TestExpressionModel>() );
-        }
-
-        [Fact]
-        public void TestAugmented()
-        {
-            LogEventData data = LogEventData.Create( new { Name1 = "Value", Name2 = 2 } ).WithAdditionalProperty( "Name3", 1.2 );
-
-            IReadOnlyDictionary<string, object> properties = data.ToDictionary();
-
-            Assert.Equal( 3, properties.Count );
-            Assert.Equal( "Value", properties["Name1"] );
-            Assert.Equal( 2, properties["Name2"] );
-            Assert.Equal( 1.2, properties["Name3"] );
-        }
-
-        [Fact]
-        public void TestAugmentedEmpty()
-        {
-            LogEventData data = default( LogEventData ).WithAdditionalProperty( "Name3", 1.2 );
-
-            IReadOnlyDictionary<string, object> properties = data.ToDictionary();
-
-            Assert.Equal( 1, properties.Count );
-            Assert.Equal( 1.2, properties["Name3"] );
-        }
-
-        [Fact]
-        public void TestAugmentedExpressionModel()
-        {
-            object rawData = new object();
-            LogEventData data = LogEventData.Create( rawData, new TestMetadata() ).WithAdditionalProperty( "Name3", 1.2 );
-
-            TestExpressionModel expressionModel = data.GetExpressionModel<TestExpressionModel>();
-            Assert.NotNull( expressionModel );
-            Assert.Same( rawData, expressionModel.Data );
-        }
+    private class PropertiesWithAttributes
+    {
+        [LoggingPropertyOptions( IsIgnored = true )]
+        public string Ignored { get; set; }
 
 
-        [Fact]
-        public void TestAnnotatedProperties()
-        {
-            PropertiesWithAttributes o = new PropertiesWithAttributes();
-            LogEventData eventData = LogEventData.Create( o );
+        [LoggingPropertyOptions( IsInherited = true )]
+        public string Inherited { get; set; }
 
-            // Test the dynamic factory.
-            Assert.Same( eventData.Metadata, LogEventData.Create( o, null ).Metadata );
-
-            Assert.True( eventData.Metadata.GetPropertyOptions( "Inherited" ).IsInherited );
-            Assert.True( eventData.Metadata.GetPropertyOptions( "Ignored" ).IsIgnored );
-
-            IReadOnlyDictionary<string, object> properties = eventData.ToDictionary();
-
-            Assert.Equal( 1, properties.Count );
-
-        }
-
-        class TestExpressionModel
-        {
-            public readonly object Data;
-
-            public TestExpressionModel( object data )
-            {
-                this.Data = data;
-            }
-        }
-
-        private class TestMetadata : LogEventMetadata<TestExpressionModel>
-        {
-            public TestMetadata() : base( "Test" )
-            {
-            }
-
-            public override TestExpressionModel GetExpressionModel( object data )
-            {
-                return new TestExpressionModel( data );
-            }
-
-        }
-
-        private class PropertiesWithAttributes
-        {
-            [LoggingPropertyOptions( IsIgnored = true )]
-            public string Ignored { get; set; }
-
-
-            [LoggingPropertyOptions( IsInherited = true )]
-            public string Inherited { get; set; }
-
-        }
     }
 }
--- src/tests/Flashtrace.UnitTests/TestDefaultLogger.cs
+++ src/tests/Flashtrace.UnitTests/TestDefaultLogger.cs
@@ -1,221 +1,143 @@
-﻿#if !NETCOREAPP1_1 && SYSTEM_TRACE // TraceSource is in .NET Core 1.1 but not in .NET Standard 1.3
-#define TRACE_SOURCE_LOGGER
-#endif
-
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using PostSharp.Patterns.Common.Tests.Helpers;
+﻿using System.Diagnostics;
 using Xunit;
-using PostSharp.Patterns.Diagnostics;
-using PostSharp.Patterns.Diagnostics.Custom;
 
-namespace PostSharp.Patterns.Common.Tests.Diagnostics
+namespace Flashtrace.UnitTests;
+
+public class TestDefaultLogger : IClassFixture<TestDefaultLogger.TraceSourceFixture>
 {
-    public class TestDefaultLogger : Xunit.IClassFixture<TestDefaultLogger.TraceSourceFixture>
+    public class TraceSourceFixture
     {
-        public class TraceSourceFixture
-        {
-#if TRACE_SOURCE_LOGGER
-            public readonly TraceSource TraceSource = TraceSourceLogger.GetTraceSource();
-            public readonly MyListener Listener = new MyListener();
-#endif
-#pragma warning disable 618
-            public Logger Logger = Logger.GetLogger();
-#pragma warning restore 618
-            public LogSource LogSource = LogSource.Get();
+        public readonly TraceSource TraceSource = TraceSourceLogger.GetTraceSource();
+        public readonly MyListener Listener = new MyListener();
 
-            public TraceSourceFixture()
-            {
-#if TRACE_SOURCE_LOGGER
-                this.TraceSource.Listeners.Add( this.Listener );
-                this.TraceSource.Switch.Level = SourceLevels.All;
-#endif
-            }
+        public LogSource LogSource = LogSource.Get();
+
+        public TraceSourceFixture()
+        {
+            this.TraceSource.Listeners.Add( this.Listener );
+            this.TraceSource.Switch.Level = SourceLevels.All;
+        }
+    }
+
+    private readonly TraceSourceFixture fixture;
+
+    public TestDefaultLogger( TraceSourceFixture fixture )
+    {
+        this.fixture = fixture;
+        this.fixture.Listener.Messages.Clear();
+    }
+
+    [Fact]
+    public void Test_TextLogger_Write()
+    {
+        this.fixture.LogSource.WithLevel( LogLevel.Error ).Write( FormattedMessageBuilder.Formatted( "Error" ) );
+        this.fixture.LogSource.WithLevel( LogLevel.Warning ).Write(FormattedMessageBuilder.Formatted("Warning"));
+        this.fixture.LogSource.WithLevel( LogLevel.Critical ).Write(FormattedMessageBuilder.Formatted("Critical"));
+        this.fixture.LogSource.WithLevel( LogLevel.Debug ).Write(FormattedMessageBuilder.Formatted("Debug"));
+        this.fixture.LogSource.WithLevel( LogLevel.Trace ).Write(FormattedMessageBuilder.Formatted("Trace"));
+        Assert.Equal( 5, this.fixture.Listener.Messages.Count );
+        Assert.Equal( "Error", this.fixture.Listener.Messages[0] );
+    }
+
+    [Fact]
+    public void Test_TextLogger_ActivitySuccess()
+    {
+        using (var activity = this.fixture.LogSource.Default.OpenActivity(FormattedMessageBuilder.Formatted("Activity")))
+        {
+            activity.SetSuccess();
         }
 
-        private readonly TraceSourceFixture fixture;
+        Assert.Equal( 2, this.fixture.Listener.Messages.Count );
+        Assert.Equal( "Activity: Starting", this.fixture.Listener.Messages[0] );
+        Assert.Equal( "Activity: Succeeded", this.fixture.Listener.Messages[1] );
+    }
 
-#if TRACE_SOURCE_LOGGER
-        public TestDefaultLogger( TraceSourceFixture fixture )
+    [Fact]
+    public void Test_TextLogger_ActivityFailure()
+    {
+        using (var activity = this.fixture.LogSource.Default.OpenActivity(FormattedMessageBuilder.Formatted("Activity")))
         {
-            this.fixture = fixture;
-            this.fixture.Listener.Messages.Clear();
-        }
-#endif
-
-        [Fact]
-        public void Test_Logger_Write()
-        {
-            this.fixture.Logger.Write(LogLevel.Error, "Error" );
-            this.fixture.Logger.Write(LogLevel.Warning, "Warning");
-            this.fixture.Logger.Write(LogLevel.Critical, "Critical");
-            this.fixture.Logger.Write(LogLevel.Debug, "Debug");
-            this.fixture.Logger.Write(LogLevel.Trace, "Trace");
-#if TRACE_SOURCE_LOGGER
-            Assert.Equal(5, this.fixture.Listener.Messages.Count );
-            Assert.Equal("Error", this.fixture.Listener.Messages[0 ] );
-#endif
+            activity.SetOutcome( LogLevel.Error, FormattedMessageBuilder.Formatted("Oops: {Hello}", "Hello"));
         }
 
-        [Fact]
-        public void Test_Logger_ActivitySuccess()
-        {
-            using ( var activity = this.fixture.Logger.OpenActivity("Activity"))
-            {
-                activity.SetSuccess();
-            }
+        Assert.Equal( 2, this.fixture.Listener.Messages.Count );
+        Assert.Equal( "Activity: Starting", this.fixture.Listener.Messages[0] );
+        Assert.Equal( "Activity: Oops: Hello", this.fixture.Listener.Messages[1] );
+    }
 
-            #if TRACE_SOURCE_LOGGER
-            Assert.Equal(2, this.fixture.Listener.Messages.Count);
-            Assert.Equal("Activity: Starting", this.fixture.Listener.Messages[0]);
-            Assert.Equal("Activity: Succeeded", this.fixture.Listener.Messages[1]);
-            #endif
+    [Fact]
+    public void Test_SemanticLogger_Write()
+    {
+        this.fixture.LogSource.WithLevel( LogLevel.Error ).Write( SemanticMessageBuilder.Semantic("Error", ("a", "b"), ("b", "c") ));
+        this.fixture.LogSource.WithLevel( LogLevel.Warning ).Write( SemanticMessageBuilder.Semantic("Warning", ("a", "b") ));
+        this.fixture.LogSource.WithLevel( LogLevel.Critical ).Write( SemanticMessageBuilder.Semantic("Critical", ("a", "b") ));
+        this.fixture.LogSource.WithLevel( LogLevel.Debug ).Write( SemanticMessageBuilder.Semantic("Debug", ("a", "b") ));
+
+        // TODO: Review - tell gael about this likely C# compiler bug.
+
+        // NB: Had to add the Semantic( string name, in (string Name, object Value) parameter1 ) overload to get this to
+        //     resolve correctly, otherwise the compiler was trying (incorrectly) to use the
+        //     Semantic(string messageName, (string Name, object Value)[] parameters) overload (note there's no 'params').
+        this.fixture.LogSource.WithLevel( LogLevel.Trace ).Write( SemanticMessageBuilder.Semantic( "Trace", ("a", null) ) );
+        Assert.Equal( 5, this.fixture.Listener.Messages.Count );
+        Assert.Equal( "Error, a = b, b = c", this.fixture.Listener.Messages[0] );
+    }
+
+    [Fact]
+    public void Test_SemanticLogger_ActivitySuccess()
+    {
+        using (var activity = this.fixture.LogSource.Default.OpenActivity(SemanticMessageBuilder.Semantic("Activity")))
+        {
+            activity.SetSuccess();
         }
 
-        [Fact]
-        public void Test_Logger_ActivityFailure()
-        {
-            using (var activity = this.fixture.Logger.OpenActivity("Activity"))
-            {
-                activity.SetFailure("Oops: {Hello}", "Hello");
-            }
+        Assert.Equal( 2, this.fixture.Listener.Messages.Count );
+        Assert.Equal( "Activity: Starting", this.fixture.Listener.Messages[0] );
+        Assert.Equal( "Activity: Succeeded", this.fixture.Listener.Messages[1] );
+    }
 
-            #if TRACE_SOURCE_LOGGER
-            Assert.Equal(2, this.fixture.Listener.Messages.Count);
-            Assert.Equal("Activity: Starting", this.fixture.Listener.Messages[0]);
-            Assert.Equal("Activity: Failed, Oops: Hello", this.fixture.Listener.Messages[1]);
-            #endif
+    [Fact]
+    public void Test_SemanticLogger_ActivityFailure()
+    {
+        using ( var activity = this.fixture.LogSource.Default.OpenActivity( SemanticMessageBuilder.Semantic( "Activity", ("a", "b") ) ) )
+        {
+            activity.SetOutcome( LogLevel.Error, SemanticMessageBuilder.Semantic( "Failed", ("c", "d") ));
         }
 
-#if CS_IN_PARAMETER
-        [Fact]
-        public void Test_TextLogger_Write()
+        Assert.Equal( 2, this.fixture.Listener.Messages.Count );
+        Assert.Equal( "Activity, a = b: Starting", this.fixture.Listener.Messages[0] );
+        Assert.Equal( "Activity, a = b: Failed, c = d", this.fixture.Listener.Messages[1] );
+    }
+
+    public class MyListener : TraceListener
+    {
+        string buffer = "";
+        public readonly List<string> Messages = new List<string>();
+    
+        public override void Write(string message)
         {
-            this.fixture.LogSource.WithLevel( LogLevel.Error ).Write( FormattedMessageBuilder.Formatted( "Error" ) );
-            this.fixture.LogSource.WithLevel( LogLevel.Warning ).Write(FormattedMessageBuilder.Formatted("Warning"));
-            this.fixture.LogSource.WithLevel( LogLevel.Critical ).Write(FormattedMessageBuilder.Formatted("Critical"));
-            this.fixture.LogSource.WithLevel( LogLevel.Debug ).Write(FormattedMessageBuilder.Formatted("Debug"));
-            this.fixture.LogSource.WithLevel( LogLevel.Trace ).Write(FormattedMessageBuilder.Formatted("Trace"));
-#if TRACE_SOURCE_LOGGER
-            AssertEx.Equal( 5, this.fixture.Listener.Messages.Count );
-            AssertEx.Equal( "Error", this.fixture.Listener.Messages[0] );
-#endif
+            buffer += message;
         }
 
-        [Fact]
-        public void Test_TextLogger_ActivitySuccess()
+        public override void WriteLine(string message)
         {
-            using (var activity = this.fixture.LogSource.Default.OpenActivity(FormattedMessageBuilder.Formatted("Activity")))
-            {
-                activity.SetSuccess();
-            }
-
-#if TRACE_SOURCE_LOGGER
-            AssertEx.Equal( 2, this.fixture.Listener.Messages.Count );
-            AssertEx.Equal( "Activity: Starting", this.fixture.Listener.Messages[0] );
-            AssertEx.Equal( "Activity: Succeeded", this.fixture.Listener.Messages[1] );
-#endif
+            this.Messages.Add(buffer + message );
+            buffer = "";
+        }
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            this.Messages.Add(message );
         }
 
-        [Fact]
-        public void Test_TextLogger_ActivityFailure()
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id)
         {
-            using (var activity = this.fixture.LogSource.Default.OpenActivity(FormattedMessageBuilder.Formatted("Activity")))
-            {
-                activity.SetOutcome( LogLevel.Error, FormattedMessageBuilder.Formatted("Oops: {Hello}", "Hello"));
-            }
-
-#if TRACE_SOURCE_LOGGER
-            AssertEx.Equal( 2, this.fixture.Listener.Messages.Count );
-            AssertEx.Equal( "Activity: Starting", this.fixture.Listener.Messages[0] );
-            AssertEx.Equal( "Activity: Oops: Hello", this.fixture.Listener.Messages[1] );
-#endif
-        }
-#endif
-
-#if VALUE_TUPLE && CS_VALUE_TUPLE && POSTSHARP_DIAGNOSTICS_SEMANTIC_MESSAGE
-
-        [Fact]
-        public void Test_SemanticLogger_Write()
-        {
-            this.fixture.LogSource.WithLevel( LogLevel.Error ).Write( SemanticMessageBuilder.Semantic("Error", ("a", "b"), ("b", "c") ));
-            this.fixture.LogSource.WithLevel( LogLevel.Warning ).Write( SemanticMessageBuilder.Semantic("Warning", ("a", "b") ));
-            this.fixture.LogSource.WithLevel( LogLevel.Critical ).Write( SemanticMessageBuilder.Semantic("Critical", ("a", "b") ));
-            this.fixture.LogSource.WithLevel( LogLevel.Debug ).Write( SemanticMessageBuilder.Semantic("Debug", ("a", "b") ));
-            this.fixture.LogSource.WithLevel( LogLevel.Trace ).Write( SemanticMessageBuilder.Semantic("Trace", ("a", null) ));
-#if TRACE_SOURCE_LOGGER
-            AssertEx.Equal( 5, this.fixture.Listener.Messages.Count );
-            AssertEx.Equal( "Error, a = b, b = c", this.fixture.Listener.Messages[0] );
-#endif
+            base.TraceEvent(eventCache, source, eventType, id);
         }
 
-        [Fact]
-        public void Test_SemanticLogger_ActivitySuccess()
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
         {
-            using (var activity = this.fixture.LogSource.Default.OpenActivity(SemanticMessageBuilder.Semantic("Activity")))
-            {
-                activity.SetSuccess();
-            }
-
-#if TRACE_SOURCE_LOGGER
-            AssertEx.Equal( 2, this.fixture.Listener.Messages.Count );
-            AssertEx.Equal( "Activity: Starting", this.fixture.Listener.Messages[0] );
-            AssertEx.Equal( "Activity: Succeeded", this.fixture.Listener.Messages[1] );
-#endif
+            this.Messages.Add(string.Format(format, args));
         }
 
-        [Fact]
-        public void Test_SemanticLogger_ActivityFailure()
-        {
-            using ( var activity = this.fixture.LogSource.Default.OpenActivity( SemanticMessageBuilder.Semantic( "Activity", ("a", "b") ) ) )
-            {
-                activity.SetOutcome( LogLevel.Error, SemanticMessageBuilder.Semantic( "Failed", ("c", "d") ));
-            }
-
-#if TRACE_SOURCE_LOGGER
-            AssertEx.Equal( 2, this.fixture.Listener.Messages.Count );
-            AssertEx.Equal( "Activity, a = b: Starting", this.fixture.Listener.Messages[0] );
-            AssertEx.Equal( "Activity, a = b: Failed, c = d", this.fixture.Listener.Messages[1] );
-#endif
-        }
-
-#endif
-
-
-#if TRACE_SOURCE_LOGGER
-        public class MyListener : TraceListener
-        {
-            string buffer = "";
-            public readonly List<string> Messages = new List<string>();
-        
-            public override void Write(string message)
-            {
-                buffer += message;
-            }
-
-            public override void WriteLine(string message)
-            {
-                this.Messages.Add(buffer + message );
-                buffer = "";
-            }
-            public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
-            {
-                this.Messages.Add(message );
-            }
-
-            public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id)
-            {
-                base.TraceEvent(eventCache, source, eventType, id);
-            }
-
-            public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
-            {
-                this.Messages.Add(string.Format(format, args));
-            }
-
-        }
-#endif
     }
 }
