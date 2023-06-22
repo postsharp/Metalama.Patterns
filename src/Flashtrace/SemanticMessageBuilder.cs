2d3b376898533f9d82a3232495b85e80917a7615
Tom Glastonbury <tom@postsharp.net>
2023-06-22 12:15:15 +0100

    Add overload of SemanticMessageBuilder.Semantic to work around an apparent C# compiler bug.

--- src/Flashtrace/SemanticMessageBuilder.cs
+++ src/Flashtrace/SemanticMessageBuilder.cs
@@ -1,7 +1,6 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Flashtrace.Messages;
-using Flashtrace.Messages;
 using System.Runtime.CompilerServices;
 
 namespace Flashtrace
@@ -20,7 +19,20 @@ namespace Flashtrace
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] // To avoid copying the struct.
         // Intentionally removing the params modifier because otherwise the C# compiler picks the wrong overload which causes a performance issue.
-        public static SemanticMessageArray Semantic(string messageName, ValueTuple<string, object>[] parameters) => new SemanticMessageArray(messageName, parameters);
+        public static SemanticMessageArray Semantic(string messageName, (string Name, object Value)[] parameters) => new SemanticMessageArray(messageName, parameters);
+
+        /// <summary>
+        /// Create a semantic message with 1 parameter, using tuples, where the value is of type <see cref="object"/>.
+        /// </summary>
+        /// <param name="name">Name of the message.</param>
+        /// <typeparam name="T1">Type of the first parameter value.</typeparam>
+        /// <param name="parameter1">Name and value of the first parameter wrapped as a tuple.</param>
+        // Without this overload, the C# compiler tries to use the (string Name, object Value)[] overload for some reason and then raises an error that the type is wrong (it's not an array).
+        [MethodImpl( MethodImplOptions.AggressiveInlining )] // To avoid copying the struct.
+        public static SemanticMessage<object> Semantic( string name, in (string Name, object Value) parameter1 )
+        {
+            return new SemanticMessage<object>( name, parameter1.Name, parameter1.Value );
+        }
 
         /// <summary>
         /// Creates a semantic message with an arbitrary number of parameters.
@@ -29,7 +41,7 @@ namespace Flashtrace
         /// <param name="parameters">Array of parameters (name-value pairs).</param>
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] // To avoid copying the struct.
-        public static SemanticMessageArray Semantic(string messageName, IReadOnlyList<ValueTuple<string, object>> parameters) => new SemanticMessageArray(messageName, parameters);
+        public static SemanticMessageArray Semantic(string messageName, IReadOnlyList<(string Name, object Value)> parameters) => new SemanticMessageArray(messageName, parameters);
 
         /// <summary>
         /// Creates a semantic message without parameter.
