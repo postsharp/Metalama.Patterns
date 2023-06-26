261102b3f66faaab56be3936a1f4319c193c9f89
Tom Glastonbury <tom@postsharp.net>
2023-06-26 16:33:09 +0100

    Add package reference to System.Runtime.CompilerServices.Unsafe and use Unsafe.AsRef instead of Post.GetMutableRef.

--- src/Flashtrace/Messages/MessageHelper.cs
+++ src/Flashtrace/Messages/MessageHelper.cs
@@ -1,5 +1,7 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
+using System.Runtime.CompilerServices;
+
 namespace Flashtrace.Messages;
 
 internal static class MessageHelper
@@ -7,12 +9,7 @@ internal static class MessageHelper
     public static void Write<T>( in T message, ILogRecordBuilder recordBuilder, LogRecordItem item )
         where T : IMessage
     {
-        // TODO: [FT-Review] Use of Post.GetMutableRef.
-#if false
-            ref T mutableRef = ref Post.GetMutableRef(message);
-            mutableRef.Write(recordBuilder, item);
-#else
-        message.Write( recordBuilder, item );
-#endif
+        // TODO: Investigate modernizing message types (eg, to readonly struct) to avoid need for Unsafe.AsRef.
+        Unsafe.AsRef( message ).Write( recordBuilder, item );
     }
 }
