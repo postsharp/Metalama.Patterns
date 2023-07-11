b2a207f4071784744a2ef58fae023f02ee8c5a3d
Tom Glastonbury <tom@postsharp.net>
2023-07-11 15:05:04 +0100

    Ported OnReferencedAssembly test, although not executing yet.

---
+++ src/tests/Metalama.Patterns.Caching.AspectTests/CacheConfigurationAttributeTests/_CacheConfigurationAttributeTest.Dependency.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+namespace Metalama.Patterns.Caching.AspectTests.CacheConfigurationAttributeTests
+{
+    public static class TestValues
+    {
+        public const string defaultProfileName = CachingProfile.DefaultName;
+        public const string cacheConfigurationAttributeProfileName1 = "namespace Metalama.Patterns.Caching.AspectTests.CacheConfigurationAttributeTests.CacheConfigurationAttribute1";
+        public const string cacheConfigurationAttributeProfileName2 = "namespace Metalama.Patterns.Caching.AspectTests.CacheConfigurationAttributeTests.CacheConfigurationAttribute2";
+    }
+
+    public class ReferencedParentCachingClass
+    {
+        public class ReferencedInnerCachingClassInBase
+        {
+            [Caching.Cache]
+            public object GetValueReferencedInnerBase()
+            {
+                return null;
+            }
+        }
+
+        [Caching.Cache]
+        public object GetValueReferencedBase()
+        {
+            return null;
+        }
+    }
+
+    public class ReferencedChildCachingClass : ReferencedParentCachingClass
+    {
+        public class ReferencedInnerCachingClassInChild
+        {
+            [Caching.Cache]
+            public object GetValueReferencedInnerChild()
+            {
+                return null;
+            }
+        }
+
+        [Caching.Cache]
+        public object GetValueReferencedChild()
+        {
+            return null;
+        }
+    }
+
+    [Caching.CacheConfiguration( ProfileName = TestValues.cacheConfigurationAttributeProfileName2 )]
+    public class ReferencedParentCachingClassOverridden
+    {
+        public class ReferencedInnerCachingClassInBaseOverridden
+        {
+            [Caching.Cache]
+            public object GetValueReferencedInnerBase()
+            {
+                return null;
+            }
+        }
+
+        [Caching.Cache]
+        public object GetValueReferencedBase()
+        {
+            return null;
+        }
+    }
+
+    public class ReferencedChildCachingClassOverridden : ReferencedParentCachingClassOverridden
+    {
+        public class ReferencedInnerCachingClassInChildOverridden
+        {
+            [Caching.Cache]
+            public object GetValueReferencedInnerChild()
+            {
+                return null;
+            }
+        }
+
+        [Caching.Cache]
+        public object GetValueReferencedChild()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/tests/Metalama.Patterns.Caching.AspectTests/CacheConfigurationAttributeTests/_CacheConfigurationAttributeTest.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Patterns.Caching.TestHelpers;
+using System;
+
+namespace Metalama.Patterns.Caching.AspectTests.CacheConfigurationAttributeTests
+{
+    public static class CacheConfigurationAttributeTest
+    {
+        public static void CheckAfterCachedMethodCall(string calledMethod, TestingCacheBackend backend, ref string previousCacheKey,
+                                                        string expectedProfileName)
+        {
+            if (backend.LastCachedKey == previousCacheKey)
+            {
+                throw new Exception($"{calledMethod}: backend.LastCachedKey '{backend.LastCachedKey}' == previousCacheKey '{previousCacheKey}'");
+            }
+
+            if (backend.LastCachedItem.Configuration.ProfileName != expectedProfileName)
+            {
+                throw new Exception(
+                    $"{calledMethod}: backend.LastCachedItem.Configuration.ProfileName '{backend.LastCachedItem.Configuration.ProfileName}' != expectedProfileName '{expectedProfileName}'");
+            }
+
+            previousCacheKey = backend.LastCachedKey;
+        }
+    }
+
+    public class LocalChildCachingClass : ReferencedChildCachingClass
+    {
+        public class LocalInnerCachingClassInChild
+        {
+            [Caching.Cache]
+            public object GetValueLocalInnerChild()
+            {
+                return null;
+            }
+        }
+
+        [Caching.Cache]
+        public object GetValueLocalChild()
+        {
+            return null;
+        }
+    }
+
+    public class LocalChildCachingClassOverridden : ReferencedChildCachingClassOverridden
+    {
+        public class LocalInnerCachingClassInChildOverridden
+        {
+            [Caching.Cache]
+            public object GetValueLocalInnerChild()
+            {
+                return null;
+            }
+        }
+
+        [Caching.Cache]
+        public object GetValueLocalChild()
+        {
+            return null;
+        }
+    }
+}
