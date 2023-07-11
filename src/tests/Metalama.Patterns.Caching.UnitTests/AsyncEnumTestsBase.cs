88b120165c9387622529cbfb7b385c3f95c1ed54
Tom Glastonbury <tom@postsharp.net>
2023-07-11 17:39:43 +0100

    Add unit tests for cached IAsyncEnumerable and IAsyncEnumerator.

---
+++ src/tests/Metalama.Patterns.Caching.UnitTests/AsyncEnumTestsBase.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+#if NETCOREAPP3_0_OR_GREATER
+
+using Metalama.Patterns.Caching.TestHelpers;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Metalama.Patterns.Caching.Tests;
+
+public abstract class AsyncEnumTestsBase : IDisposable
+{
+    protected ITestOutputHelper TestOutputHelper { get; }
+
+    protected StringBuilder StringBuilder { get; }
+
+    protected TestClass Instance { get; }
+
+    public AsyncEnumTestsBase( ITestOutputHelper testOutputHelper )
+    {
+        this.TestOutputHelper = testOutputHelper;
+        this.StringBuilder = new();
+        this.Instance = new TestClass( this.Log );
+
+        TestProfileConfigurationFactory.InitializeTestWithCachingBackend( nameof( AsyncEnumerableTests ) );
+    }
+
+    public void Dispose()
+    {
+        TestProfileConfigurationFactory.DisposeTest();
+        this.TestOutputHelper.WriteLine( this.StringBuilder.ToString() );
+    }
+
+    protected void Log( string message )
+    {
+        if ( this.StringBuilder.Length > 0 )
+        {
+            this.StringBuilder.Append( "." );
+        }
+        this.StringBuilder.Append( message );
+    }
+
+    protected async Task Iterate( IAsyncEnumerator<int> iterator )
+    {
+        this.Log( "I1" );
+        while ( await iterator.MoveNextAsync() )
+        {
+            this.Log( $"I2[{iterator.Current}]" );
+        }
+        this.Log( "I3" );
+    }
+
+    public sealed class TestClass
+    {
+        private readonly Action<string> _log;
+
+        public TestClass( Action<string> log )
+        {
+            this._log = log;
+        }
+
+        [Cache( IgnoreThisParameter = true )]
+        public async IAsyncEnumerable<int> CachedEnumerable()
+        {
+            this._log( "E1" );
+            await Task.Delay( 1 );
+            this._log( "E2" );
+            yield return 42;
+            this._log( "E3" );
+            yield return 99;
+            this._log( "E4" );
+        }
+
+        [Cache( IgnoreThisParameter = true )]
+        public async IAsyncEnumerator<int> CachedEnumerator()
+        {
+            this._log( "E1" );
+            await Task.Delay( 1 );
+            this._log( "E2" );
+            yield return 42;
+            this._log( "E3" );
+            yield return 99;
+            this._log( "E4" );
+        }
+    }
+}
+
+#endif
---
+++ src/tests/Metalama.Patterns.Caching.UnitTests/AsyncEnumerableTests.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+#if NETCOREAPP3_0_OR_GREATER
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Metalama.Patterns.Caching.Tests;
+
+public class AsyncEnumerableTests : AsyncEnumTestsBase
+{
+    public AsyncEnumerableTests( ITestOutputHelper testOutputHelper ) : base( testOutputHelper )
+    {
+    }
+
+    [Fact]
+    public void DoesNotIterateOnUnawaitedMethod()
+    {
+        _ = this.Instance.CachedEnumerable();
+
+        Assert.Equal( "E1", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public void DoesNotIterateOnGetAsyncEnumerator()
+    {
+        _ = this.Instance.CachedEnumerable().GetAsyncEnumerator();
+
+        Assert.Equal( "E1", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public void DoesNotIterateOnUnawaitedFirstMoveNextAsync()
+    {
+        _ = this.Instance.CachedEnumerable().GetAsyncEnumerator().MoveNextAsync();
+
+        Assert.Equal( "E1", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public async void IteratesCompletelyOnFirstAwaitedMoveNextAsync()
+    {
+        _ = await this.Instance.CachedEnumerable().GetAsyncEnumerator().MoveNextAsync();
+
+        Assert.Equal( "E1.E2.E3.E4", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public async void DoesNotIteratesOnSecondAwaitedMoveNextAsync()
+    {
+        _ = await this.Instance.CachedEnumerable().GetAsyncEnumerator().MoveNextAsync();
+        this.StringBuilder.Clear();
+        _ = await this.Instance.CachedEnumerable().GetAsyncEnumerator().MoveNextAsync();
+
+        Assert.Equal( "", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public async void IteratesExpectedSequence1()
+    {
+        await this.Iterate( this.Instance.CachedEnumerable().GetAsyncEnumerator() );
+
+        Assert.Equal( "E1.I1.E2.E3.E4.I2[42].I2[99].I3", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public async void IteratesExpectedSequence2()
+    {
+        await this.Iterate( this.Instance.CachedEnumerable().GetAsyncEnumerator() );
+        await this.Iterate( this.Instance.CachedEnumerable().GetAsyncEnumerator() );
+
+        Assert.Equal( "E1.I1.E2.E3.E4.I2[42].I2[99].I3.I1.I2[42].I2[99].I3", this.StringBuilder.ToString() );
+    }
+
+    [Fact]
+    public async void IteratesExpectedSequence3()
+    {
+        var seq = this.Instance.CachedEnumerable();
+        await this.Iterate( seq.GetAsyncEnumerator() );
+        await this.Iterate( seq.GetAsyncEnumerator() );
+
+        Assert.Equal( "E1.I1.E2.E3.E4.I2[42].I2[99].I3.I1.I2[42].I2[99].I3", this.StringBuilder.ToString() );
+    }
+}
+
+#endif
