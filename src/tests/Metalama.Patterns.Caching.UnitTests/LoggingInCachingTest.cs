7c4a83fa7456580c45c3bd945254ac561097019e
Tom Glastonbury <tom@postsharp.net>
2023-07-14 11:58:24 +0100

    Clean up disabled unit tests of logging in caching.
    These tests rely on PostSharp's TextLoggingBackend. Also, the concept of logging within caching is under review. So not fixing for now.

--- src/tests/Metalama.Patterns.Caching.UnitTests/LoggingInCachingTest.cs
+++ src/tests/Metalama.Patterns.Caching.UnitTests/LoggingInCachingTest.cs
@@ -1,14 +1,46 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-// TODO: !!! [Porting] Disabled: Logging in caching tests currently rely on TextLoggingBackend.
+// TODO: [Porting] Disabled: Logging in caching tests currently rely on TextLoggingBackend.
 
 #if false
+
 using Metalama.Patterns.Caching.TestHelpers;
 using System;
 using Xunit;
 
 namespace Metalama.Patterns.Caching.Tests
 {
+    public class InitializationAndCleanup
+    {
+        [ModuleInitializer( 0 )]
+        public static void AssemblyInitialize()
+        {
+            Trace.Listeners.Add( new TextWriterTraceListener( Console.Out ) );
+        }
+    }
+
+    internal static class LoggingInitializer
+    {
+        [ModuleInitializer( 1 )]
+        public static void Initialize()
+        {
+            Assert.False( LoggingServices.IsRuntimeLicenseExpired );
+            LoggingServices.DefaultBackend = new ConsoleLoggingBackend();
+
+            // By default, logging is disabled:
+            Assert.False(
+                LoggingServices.DefaultBackend.CurrentContextLocalConfiguration.Verbosity.GetSource( LoggingRoles.Caching, typeof(LoggingInitializer) )
+                    .IsEnabled( LogLevel.Debug ) );
+
+            // But we enable it:
+            LoggingServices.DefaultBackend.DefaultContextLocalConfiguration.Verbosity.SetMinimalLevel( LogLevel.Debug, LoggingRoles.Caching );
+
+            Assert.True(
+                LoggingServices.DefaultBackend.CurrentContextLocalConfiguration.Verbosity.GetSource( LoggingRoles.Caching, typeof(LoggingInitializer) )
+                    .IsEnabled( LogLevel.Debug ) );
+        }
+    }
+
     public class LoggingInCachingTest : IDisposable
     {
         [Fact]
