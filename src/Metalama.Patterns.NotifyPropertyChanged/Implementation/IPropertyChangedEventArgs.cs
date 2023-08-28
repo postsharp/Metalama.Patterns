208dd0c401a73989111c8abd0c2feddf7464efe0
Tom Glastonbury <tom@postsharp.net>
2023-08-28 14:28:15 +0100

    Don't make IPropertyChangedEventArgs implement INotifyPropertyChanged, it makes no sense.

--- src/Metalama.Patterns.NotifyPropertyChanged/Implementation/IPropertyChangedEventArgs.cs
+++ src/Metalama.Patterns.NotifyPropertyChanged/Implementation/IPropertyChangedEventArgs.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-using System.ComponentModel;
-
 namespace Metalama.Patterns.NotifyPropertyChanged.Implementation;
 
-public interface IPropertyChangedEventArgs<out T> : INotifyPropertyChanged
+/// <summary>
+/// Allows covariant use of <see cref="PropertyChangedEventArgs{T}"/>.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IPropertyChangedEventArgs<out T>
 {
     T? OldValue { get; }
 
