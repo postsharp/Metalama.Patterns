ceedc294eb9ae3663a1d1cfe654d6c3826241044
Tom Glastonbury <tom@postsharp.net>
2023-10-04 17:52:10 +0100

    Move Contracts diagnostics aspect tests to Diagnostics folder/namespace.

--- src/tests/Metalama.Patterns.Contracts.AspectTests/Diagnostics/Range_InvalidType_Byte.cs
+++ src/tests/Metalama.Patterns.Contracts.AspectTests/Diagnostics/Range_InvalidType_Byte.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-namespace Metalama.Patterns.Contracts.AspectTests;
+namespace Metalama.Patterns.Contracts.AspectTests.Diagnostics;
 
-public class Range_InvalidType_Byte4
+public class Range_InvalidType_Byte
 {
-    [GreaterThan( 256.0 )]
+    [GreaterThan( 256 )]
     private byte field;
 }
