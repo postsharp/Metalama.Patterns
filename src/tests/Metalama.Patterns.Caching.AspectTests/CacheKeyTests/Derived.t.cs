5bfc5819946fbeb42b989cf3bba819bdee6028a5
GaelFraiteur <gael@postsharp.net>
2023-10-10 10:15:40 +0200

    Added missing file.

---
+++ src/tests/Metalama.Patterns.Caching.AspectTests/CacheKeyTests/Derived.t.cs
@@ -0,0 +1,37 @@
+using Flashtrace.Formatters;
+using Metalama.Patterns.Caching.Aspects;
+using Metalama.Patterns.Caching.Formatters;
+namespace Metalama.Patterns.Caching.AspectTests.CacheKeyTests.Derived;
+public class BaseClass : IFormattable<CacheKeyFormatting>
+{
+    [CacheKey]
+    public string Id { get; }
+    public string? Description { get; }
+    void IFormattable<CacheKeyFormatting>.Format(UnsafeStringBuilder stringBuilder, IFormatterRepository formatterRepository)
+    {
+        this.FormatCacheKey(stringBuilder, formatterRepository);
+    }
+    protected virtual void FormatCacheKey(UnsafeStringBuilder stringBuilder, IFormatterRepository formatterRepository)
+    {
+        stringBuilder.Append(this.GetType().FullName);
+        if (formatterRepository.Role is CacheKeyFormatting)
+        {
+            stringBuilder.Append(" ");
+            formatterRepository.Get<string>().Write(stringBuilder, this.Id);
+        }
+    }
+}
+public class DerivedClass : BaseClass
+{
+    [CacheKey]
+    public int SubId { get; }
+    protected override void FormatCacheKey(UnsafeStringBuilder stringBuilder, IFormatterRepository formatterRepository)
+    {
+        base.FormatCacheKey(stringBuilder, formatterRepository);
+        if (formatterRepository.Role is CacheKeyFormatting)
+        {
+            stringBuilder.Append(" ");
+            formatterRepository.Get<int>().Write(stringBuilder, this.SubId);
+        }
+    }
+}
