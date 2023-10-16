af40e48be390edbc2a3ca3f96e5b2cdab22497ec
Tom Glastonbury <tom@postsharp.net>
2023-10-16 16:11:04 +0100

    Accept test output having previously changed diagnostic message.

--- src/tests/Metalama.Patterns.NotifyPropertyChanged.AspectTests/Diagnostics/CallUnsafeMethodOfExternalClass.t.cs
+++ src/tests/Metalama.Patterns.NotifyPropertyChanged.AspectTests/Diagnostics/CallUnsafeMethodOfExternalClass.t.cs
@@ -1,1 +1,1 @@
-// Warning LAMA5162 on `ExternalClass.Foo()`: `Method ExternalClass.Foo() cannot be analysed, and has not been configured as safe to call. Use [SafeForDependencyAnalysis] or NotifyPropertyChangedOptionsBuilder via a fabric to configure ExternalClass.Foo() as safe to call.`
+// Warning LAMA5162 on `ExternalClass.Foo()`: `Method ExternalClass.Foo() cannot be analysed, and has not been configured as safe to call. Use [SafeForDependencyAnalysis] or ConfigureDependencyAnalysis via a fabric to configure ExternalClass.Foo() as safe to call.`
