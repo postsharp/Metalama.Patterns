335762f6d4cf2da363c2bff8de512223acf4ce2e
Tom Glastonbury <tom@postsharp.net>
2023-10-17 11:37:12 +0100

    Accept test output.

--- src/tests/Metalama.Patterns.NotifyPropertyChanged.AspectTests/Diagnostics/CallUnsafeMethodOfExternalClass.t.cs
+++ src/tests/Metalama.Patterns.NotifyPropertyChanged.AspectTests/Diagnostics/CallUnsafeMethodOfExternalClass.t.cs
@@ -1,1 +1,1 @@
-// Pending TP-33954: Warning LAMA5162 on `ExternalClass.Foo()`: `Method ExternalClass.Foo() cannot be analysed, and has not been configured as safe to call. Use [SafeToCallForDependencyAnalysis] or NotifyPropertyChangedOptionsBuilder via a fabric to configure ExternalClass.Foo() as safe to call.`
+// Warning LAMA5162 on `ExternalClass.Foo()`: `Method ExternalClass.Foo() cannot be analysed, and has not been configured as safe to call. Use [SafeToCallForDependencyAnalysis] or NotifyPropertyChangedOptionsBuilder via a fabric to configure ExternalClass.Foo() as safe to call.`
