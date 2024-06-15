13c9d1cb801b4f3aac2d096d7e1daec9a112479d
GaelFraiteur <gael@postsharp.net>
2024-06-15 12:30:38 +0200

    Accepting test outputs.

--- src/tests/Metalama.Patterns.Contracts.AspectTests/Ambiguities/Unspecified_Strict.t.cs
+++ src/tests/Metalama.Patterns.Contracts.AspectTests/Ambiguities/Unspecified_Strict.t.cs
@@ -1,94 +1,79 @@
-// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
-
 using System;
 using Metalama.Framework.Fabrics;
-
 namespace Metalama.Patterns.Contracts.AspectTests.Ambiguities.Unspecified_Strict;
-
 public class C
 {
-    private int _a;
-
-    [Positive]
-    public int A
+  private int _a;
+  [Positive]
+  public int A
+  {
+    get
     {
-        get
-        {
-            return this._a;
-        }
-        set
-        {
-            if ( value is <= 0 )
-            {
-                throw new ArgumentOutOfRangeException( "value", "The 'A' property must be strictly greater than 0." );
-            }
-
-            this._a = value;
-        }
+      return _a;
     }
-
-    private int _b;
-
-    [Positive]
-    public int B
+    set
     {
-        get
-        {
-            return this._b;
-        }
-        set
-        {
-            if ( value is <= 0 )
-            {
-                throw new ArgumentOutOfRangeException( "value", "The 'B' property must be strictly greater than 0." );
-            }
-
-            this._b = value;
-        }
+      if (value is <= 0)
+      {
+        throw new ArgumentOutOfRangeException("value", "The 'A' property must be strictly greater than 0.");
+      }
+      _a = value;
     }
-
-    private int _d;
-
-    [GreaterThan( 5 )]
-    public int D
+  }
+  private int _b;
+  [Positive]
+  public int B
+  {
+    get
     {
-        get
-        {
-            return this._d;
-        }
-        set
-        {
-            if ( value is <= 5 )
-            {
-                throw new ArgumentOutOfRangeException( "value", "The 'D' property must be strictly greater than 5." );
-            }
-
-            this._d = value;
-        }
+      return _b;
     }
-
-    private int _e;
-
-    [LessThan( 5 )]
-    public int E
+    set
     {
-        get
-        {
-            return this._e;
-        }
-        set
-        {
-            if ( value is >= 5 )
-            {
-                throw new ArgumentOutOfRangeException( "value", "The 'E' property must be strictly less than 5." );
-            }
-
-            this._e = value;
-        }
+      if (value is <= 0)
+      {
+        throw new ArgumentOutOfRangeException("value", "The 'B' property must be strictly greater than 0.");
+      }
+      _b = value;
     }
+  }
+  private int _d;
+  [GreaterThan(5)]
+  public int D
+  {
+    get
+    {
+      return _d;
+    }
+    set
+    {
+      if (value is <= 5)
+      {
+        throw new ArgumentOutOfRangeException("value", "The 'D' property must be strictly greater than 5.");
+      }
+      _d = value;
+    }
+  }
+  private int _e;
+  [LessThan(5)]
+  public int E
+  {
+    get
+    {
+      return _e;
+    }
+    set
+    {
+      if (value is >= 5)
+      {
+        throw new ArgumentOutOfRangeException("value", "The 'E' property must be strictly less than 5.");
+      }
+      _e = value;
+    }
+  }
 }
 #pragma warning disable CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
 internal class Fabric : ProjectFabric
 {
-    public override void AmendProject( IProjectAmender amender ) => throw new NotSupportedException( "Compile-time-only code cannot be called at run-time." );
+  public override void AmendProject(IProjectAmender amender) => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
 }
