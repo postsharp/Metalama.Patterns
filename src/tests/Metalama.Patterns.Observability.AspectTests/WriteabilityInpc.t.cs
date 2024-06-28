cf8e5ddd5947317255cf9387a839f4416c458d95
Petr Onderka <petro@postsharp.net>
2024-06-28 14:59:45 +0200

    #35005 Subscribe to property changed of readonly observable child objects

---
+++ src/tests/Metalama.Patterns.Observability.AspectTests/WriteabilityInpc.t.cs
@@ -0,0 +1,413 @@
+using System.ComponentModel;
+namespace Metalama.Patterns.Observability.AspectTests.WriteabilityInpc;
+[Observable]
+public class Person : INotifyPropertyChanged
+{
+  private string? _firstName;
+  public string? FirstName
+  {
+    get
+    {
+      return _firstName;
+    }
+    set
+    {
+      if (!object.ReferenceEquals(value, _firstName))
+      {
+        _firstName = value;
+        OnPropertyChanged("FirstName");
+      }
+    }
+  }
+  private string? _lastName;
+  public string? LastName
+  {
+    get
+    {
+      return _lastName;
+    }
+    set
+    {
+      if (!object.ReferenceEquals(value, _lastName))
+      {
+        _lastName = value;
+        OnPropertyChanged("LastName");
+      }
+    }
+  }
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelField : INotifyPropertyChanged
+{
+  private Person _model1 = default !;
+  private Person _model
+  {
+    get
+    {
+      return _model1;
+    }
+    set
+    {
+      if (!object.ReferenceEquals(value, _model1))
+      {
+        var oldValue = _model1;
+        if (oldValue != null)
+        {
+          oldValue.PropertyChanged -= _handle_modelPropertyChanged;
+        }
+        _model1 = value;
+        OnPropertyChanged("FirstName");
+        OnPropertyChanged("FullName");
+        OnPropertyChanged("LastName");
+        SubscribeTo_model(value);
+      }
+    }
+  }
+  public PersonViewModelField(Person model)
+  {
+    this._model = model;
+  }
+  public string? FirstName => this._model.FirstName;
+  public string? LastName => this._model.LastName;
+  public string FullName => $"{this.FirstName} {this.LastName}";
+  private PropertyChangedEventHandler? _handle_modelPropertyChanged;
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  private void SubscribeTo_model(Person value)
+  {
+    if (value != null)
+    {
+      _handle_modelPropertyChanged ??= HandlePropertyChanged;
+      value.PropertyChanged += _handle_modelPropertyChanged;
+    }
+    void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+      {
+        var propertyName = e.PropertyName;
+        switch (propertyName)
+        {
+          case "FirstName":
+            OnPropertyChanged("FirstName");
+            OnPropertyChanged("FullName");
+            break;
+          case "LastName":
+            OnPropertyChanged("FullName");
+            OnPropertyChanged("LastName");
+            break;
+        }
+      }
+    }
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelReadonlyField : INotifyPropertyChanged
+{
+  private readonly Person _model1 = default !;
+  private Person _model
+  {
+    get
+    {
+      return _model1;
+    }
+    init
+    {
+      if (!object.ReferenceEquals(value, _model1))
+      {
+        var oldValue = _model1;
+        if (oldValue != null)
+        {
+          oldValue.PropertyChanged -= _handle_modelPropertyChanged;
+        }
+        _model1 = value;
+        OnPropertyChanged("FirstName");
+        OnPropertyChanged("FullName");
+        OnPropertyChanged("LastName");
+        SubscribeTo_model(value);
+      }
+    }
+  }
+  public PersonViewModelReadonlyField(Person model)
+  {
+    this._model = model;
+  }
+  public string? FirstName => this._model.FirstName;
+  public string? LastName => this._model.LastName;
+  public string FullName => $"{this.FirstName} {this.LastName}";
+  private PropertyChangedEventHandler? _handle_modelPropertyChanged;
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  private void SubscribeTo_model(Person value)
+  {
+    if (value != null)
+    {
+      _handle_modelPropertyChanged ??= HandlePropertyChanged;
+      value.PropertyChanged += _handle_modelPropertyChanged;
+    }
+    void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+      {
+        var propertyName = e.PropertyName;
+        switch (propertyName)
+        {
+          case "FirstName":
+            OnPropertyChanged("FirstName");
+            OnPropertyChanged("FullName");
+            break;
+          case "LastName":
+            OnPropertyChanged("FullName");
+            OnPropertyChanged("LastName");
+            break;
+        }
+      }
+    }
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelConst : INotifyPropertyChanged
+{
+  private const Person? _model = null;
+  public string? FirstName => _model?.FirstName;
+  public string? LastName => _model?.LastName;
+  public string FullName => $"{this.FirstName} {this.LastName}";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelGetOnlyProperty : INotifyPropertyChanged
+{
+  private readonly Person _model = default !;
+  private Person Model
+  {
+    get
+    {
+      return _model;
+    }
+    init
+    {
+      if (!object.ReferenceEquals(value, _model))
+      {
+        var oldValue = _model;
+        if (oldValue != null)
+        {
+          oldValue.PropertyChanged -= _handleModelPropertyChanged;
+        }
+        _model = value;
+        OnObservablePropertyChanged("Model", oldValue, (INotifyPropertyChanged? )value);
+        OnPropertyChanged("FirstName");
+        OnPropertyChanged("FullName");
+        OnPropertyChanged("LastName");
+        SubscribeToModel(value);
+      }
+    }
+  }
+  public PersonViewModelGetOnlyProperty(Person model)
+  {
+    this.Model = model;
+  }
+  public string? FirstName => this.Model.FirstName;
+  public string? LastName => this.Model.LastName;
+  public string FullName => $"{this.FirstName} {this.LastName}";
+  private PropertyChangedEventHandler? _handleModelPropertyChanged;
+  [ObservedExpressions("Model")]
+  protected virtual void OnChildPropertyChanged(string parentPropertyPath, string propertyName)
+  {
+  }
+  [ObservedExpressions("Model")]
+  protected virtual void OnObservablePropertyChanged(string propertyPath, INotifyPropertyChanged? oldValue, INotifyPropertyChanged? newValue)
+  {
+  }
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  private void SubscribeToModel(Person value)
+  {
+    if (value != null)
+    {
+      _handleModelPropertyChanged ??= HandlePropertyChanged;
+      value.PropertyChanged += _handleModelPropertyChanged;
+    }
+    void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+      {
+        var propertyName = e.PropertyName;
+        switch (propertyName)
+        {
+          case "FirstName":
+            OnPropertyChanged("FirstName");
+            OnPropertyChanged("FullName");
+            break;
+          case "LastName":
+            OnPropertyChanged("FullName");
+            OnPropertyChanged("LastName");
+            break;
+        }
+      }
+    }
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelInitProperty : INotifyPropertyChanged
+{
+  private readonly Person _model = default !;
+  private Person Model
+  {
+    get
+    {
+      return _model;
+    }
+    init
+    {
+      if (!object.ReferenceEquals(value, _model))
+      {
+        var oldValue = _model;
+        if (oldValue != null)
+        {
+          oldValue.PropertyChanged -= _handleModelPropertyChanged;
+        }
+        _model = value;
+        OnObservablePropertyChanged("Model", oldValue, (INotifyPropertyChanged? )value);
+        OnPropertyChanged("FirstName");
+        OnPropertyChanged("FullName");
+        OnPropertyChanged("LastName");
+        SubscribeToModel(value);
+      }
+    }
+  }
+  public PersonViewModelInitProperty(Person model)
+  {
+    this.Model = model;
+  }
+  public string? FirstName => this.Model.FirstName;
+  public string? LastName => this.Model.LastName;
+  public string FullName => $"{this.FirstName} {this.LastName}";
+  private PropertyChangedEventHandler? _handleModelPropertyChanged;
+  [ObservedExpressions("Model")]
+  protected virtual void OnChildPropertyChanged(string parentPropertyPath, string propertyName)
+  {
+  }
+  [ObservedExpressions("Model")]
+  protected virtual void OnObservablePropertyChanged(string propertyPath, INotifyPropertyChanged? oldValue, INotifyPropertyChanged? newValue)
+  {
+  }
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  private void SubscribeToModel(Person value)
+  {
+    if (value != null)
+    {
+      _handleModelPropertyChanged ??= HandlePropertyChanged;
+      value.PropertyChanged += _handleModelPropertyChanged;
+    }
+    void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+      {
+        var propertyName = e.PropertyName;
+        switch (propertyName)
+        {
+          case "FirstName":
+            OnPropertyChanged("FirstName");
+            OnPropertyChanged("FullName");
+            break;
+          case "LastName":
+            OnPropertyChanged("FullName");
+            OnPropertyChanged("LastName");
+            break;
+        }
+      }
+    }
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelProperty : INotifyPropertyChanged
+{
+  private Person _model = default !;
+  private Person Model
+  {
+    get
+    {
+      return _model;
+    }
+    set
+    {
+      if (!object.ReferenceEquals(value, _model))
+      {
+        var oldValue = _model;
+        if (oldValue != null)
+        {
+          oldValue.PropertyChanged -= _handleModelPropertyChanged;
+        }
+        _model = value;
+        OnObservablePropertyChanged("Model", oldValue, (INotifyPropertyChanged? )value);
+        OnPropertyChanged("FirstName");
+        OnPropertyChanged("FullName");
+        OnPropertyChanged("LastName");
+        SubscribeToModel(value);
+      }
+    }
+  }
+  public PersonViewModelProperty(Person model)
+  {
+    this.Model = model;
+  }
+  public string? FirstName => this.Model.FirstName;
+  public string? LastName => this.Model.LastName;
+  public string FullName => $"{this.FirstName} {this.LastName}";
+  private PropertyChangedEventHandler? _handleModelPropertyChanged;
+  [ObservedExpressions("Model")]
+  protected virtual void OnChildPropertyChanged(string parentPropertyPath, string propertyName)
+  {
+  }
+  [ObservedExpressions("Model")]
+  protected virtual void OnObservablePropertyChanged(string propertyPath, INotifyPropertyChanged? oldValue, INotifyPropertyChanged? newValue)
+  {
+  }
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  private void SubscribeToModel(Person value)
+  {
+    if (value != null)
+    {
+      _handleModelPropertyChanged ??= HandlePropertyChanged;
+      value.PropertyChanged += _handleModelPropertyChanged;
+    }
+    void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+      {
+        var propertyName = e.PropertyName;
+        switch (propertyName)
+        {
+          case "FirstName":
+            OnPropertyChanged("FirstName");
+            OnPropertyChanged("FullName");
+            break;
+          case "LastName":
+            OnPropertyChanged("FullName");
+            OnPropertyChanged("LastName");
+            break;
+        }
+      }
+    }
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
---
+++ src/tests/Metalama.Patterns.Observability.AspectTests/WriteabilityNonInpc.t.cs
@@ -0,0 +1,174 @@
+using System;
+using System.ComponentModel;
+namespace Metalama.Patterns.Observability.AspectTests.WriteabilityNonInpc;
+[Observable]
+public class PersonViewModelField : INotifyPropertyChanged
+{
+  private string _name1 = default !;
+  private string _name
+  {
+    get
+    {
+      return _name1;
+    }
+    set
+    {
+      if (!object.ReferenceEquals(value, _name1))
+      {
+        _name1 = value;
+        OnPropertyChanged("Description");
+        OnPropertyChanged("Name");
+      }
+    }
+  }
+  private DateOnly _dateOfBirth1;
+  private DateOnly _dateOfBirth
+  {
+    get
+    {
+      return _dateOfBirth1;
+    }
+    set
+    {
+      if (_dateOfBirth1 != value)
+      {
+        _dateOfBirth1 = value;
+        OnPropertyChanged("DateOfBirth");
+      }
+    }
+  }
+  public PersonViewModelField(string name, DateOnly dateOfBirth)
+  {
+    this._name = name;
+    this._dateOfBirth = dateOfBirth;
+  }
+  public string Name => this._name;
+  public DateOnly DateOfBirth => this._dateOfBirth;
+  public string Description => $"{this.Name} (b. {this.DateOfBirth.Year})";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelReadonlyField : INotifyPropertyChanged
+{
+  private readonly string _name;
+  private readonly DateOnly _dateOfBirth;
+  public PersonViewModelReadonlyField(string name, DateOnly dateOfBirth)
+  {
+    this._name = name;
+    this._dateOfBirth = dateOfBirth;
+  }
+  public string Name => this._name;
+  public DateOnly DateOfBirth => this._dateOfBirth;
+  public string Description => $"{this.Name} (b. {this.DateOfBirth.Year})";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelConst : INotifyPropertyChanged
+{
+  private const string _name = "Adele";
+  private const int _yearOfBirth = 1988;
+  public string Name => _name;
+  public int YearOfBirth => _yearOfBirth;
+  public string Description => $"{this.Name} (b. {this.YearOfBirth})";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelGetOnlyProperty : INotifyPropertyChanged
+{
+  private string Name0 { get; }
+  private DateOnly DateOfBirth0 { get; }
+  public PersonViewModelGetOnlyProperty(string name, DateOnly dateOfBirth)
+  {
+    this.Name0 = name;
+    this.DateOfBirth0 = dateOfBirth;
+  }
+  public string Name => this.Name0;
+  public DateOnly DateOfBirth => this.DateOfBirth0;
+  public string Description => $"{this.Name} (b. {this.DateOfBirth.Year})";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelInitProperty : INotifyPropertyChanged
+{
+  private string Name0 { get; init; }
+  private DateOnly DateOfBirth0 { get; init; }
+  public PersonViewModelInitProperty(string name, DateOnly dateOfBirth)
+  {
+    this.Name0 = name;
+    this.DateOfBirth0 = dateOfBirth;
+  }
+  public string Name => this.Name0;
+  public DateOnly DateOfBirth => this.DateOfBirth0;
+  public string Description => $"{this.Name} (b. {this.DateOfBirth.Year})";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
+[Observable]
+public class PersonViewModelProperty : INotifyPropertyChanged
+{
+  private string _name0 = default !;
+  private string Name0
+  {
+    get
+    {
+      return _name0;
+    }
+    set
+    {
+      if (!object.ReferenceEquals(value, _name0))
+      {
+        _name0 = value;
+        OnPropertyChanged("Description");
+        OnPropertyChanged("Name");
+      }
+    }
+  }
+  private DateOnly _dateOfBirth0;
+  private DateOnly DateOfBirth0
+  {
+    get
+    {
+      return _dateOfBirth0;
+    }
+    set
+    {
+      if (_dateOfBirth0 != value)
+      {
+        _dateOfBirth0 = value;
+        OnPropertyChanged("DateOfBirth");
+      }
+    }
+  }
+  public PersonViewModelProperty(string name, DateOnly dateOfBirth)
+  {
+    this.Name0 = name;
+    this.DateOfBirth0 = dateOfBirth;
+  }
+  public string Name => this.Name0;
+  public DateOnly DateOfBirth => this.DateOfBirth0;
+  public string Description => $"{this.Name} (b. {this.DateOfBirth.Year})";
+  protected virtual void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+}
