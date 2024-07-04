508f7ff6a10e1ccac973289583ad2af995fd6975
GaelFraiteur <gael@postsharp.net>
2024-07-04 12:44:47 +0200

    Warnings.

--- src/tests/Metalama.Patterns.Caching.UnitTests/Backends/Single/RedisBackendObserver.cs
+++ src/tests/Metalama.Patterns.Caching.UnitTests/Backends/Single/RedisBackendObserver.cs
@@ -1,8 +1,6 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Patterns.Caching.Backends.Redis;
-using Metalama.Patterns.Caching.Implementation;
-using System.Collections.Concurrent;
 
 namespace Metalama.Patterns.Caching.Tests.Backends.Single;
 
