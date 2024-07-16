e5f9c3937b05b9b8afa493a09228f01ab2653710
Petr Onderka <petro@postsharp.net>
2024-07-16 12:12:38 +0200

    #35193 Added actual value to ArgumentOutOfRangeException in contracts

--- src/tests/Metalama.Patterns.Contracts.AspectTests/ObjectRange.t.cs
+++ src/tests/Metalama.Patterns.Contracts.AspectTests/ObjectRange.t.cs
@@ -7,22 +7,22 @@ internal class C
   {
     if (global::Metalama.Patterns.Contracts.Numeric.NumberComparer.IsStrictlySmallerThan(a, 0) == true)
     {
-      throw new ArgumentOutOfRangeException("a", "The 'a' parameter must be greater than or equal to 0.");
+      throw new ArgumentOutOfRangeException("a", a, "The 'a' parameter must be greater than or equal to 0.");
     }
     if (global::Metalama.Patterns.Contracts.Numeric.NumberComparer.IsStrictlySmallerThan(b, 0) == true || global::Metalama.Patterns.Contracts.Numeric.NumberComparer.IsStrictlyGreaterThan(b, 100) == true)
     {
-      throw new ArgumentOutOfRangeException("The 'b' parameter must be in the range [0, 100].", "b");
+      throw new ArgumentOutOfRangeException("b", b, "The 'b' parameter must be in the range [0, 100].");
     }
     object returnValue;
     c = a;
     returnValue = b;
     if (global::Metalama.Patterns.Contracts.Numeric.NumberComparer.IsStrictlyGreaterThan(c, 1.01M) == true)
     {
-      throw new PostconditionViolationException("The 'c' parameter must be less than or equal to 1.01.");
+      throw new PostconditionViolationException("The 'c' parameter must be less than or equal to 1.01.", c);
     }
     if (global::Metalama.Patterns.Contracts.Numeric.NumberComparer.IsGreaterThan(returnValue, 0) == true)
     {
-      throw new PostconditionViolationException("The return value must be strictly greater than 0.");
+      throw new PostconditionViolationException("The return value must be strictly greater than 0.", returnValue);
     }
     return returnValue;
   }
